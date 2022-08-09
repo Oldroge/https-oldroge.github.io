f2e5d7410a3ea31c16629067035c960812c9eacb
oldroge <rogeriomrjr@gmail.com>
2022-08-09 16:42:11 -0300

    feat: create about me text component

---
+++ src/components/AboutMeText/AboutMeText.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function AboutMeText() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Sed doloremque mollitia sunt rerum neque repellat natus aliquid dolor perferendis,
+        sapiente, voluptas temporibus iste ex, quo adipisci et suscipit eligendi dolorum.
+      </p>
+    </div>
+  );
+}
