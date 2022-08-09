9f5aa7c10d99c098c3261426a3543bcf29f0d1bf
oldroge <rogeriomrjr@gmail.com>
2022-08-09 14:54:53 -0300

    feat: add tag image with Roge photo

--- src/pages/about-me/AboutMePage.jsx
+++ src/pages/about-me/AboutMePage.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 import Header from '../../components/Header/Header';
 
+import './AboutMe.css';
+import rogeImage from './images/roge-image.jpeg';
+
 export default function AboutMePage() {
   return (
-    <Header />
+    <section id="about-me-page">
+      <Header />
+      <img
+        src={rogeImage}
+        alt="Roge sorrindo olhando pra câmera e fundo branco"
+        className="roge-photo"
+      />
+    </section>
   );
 }
