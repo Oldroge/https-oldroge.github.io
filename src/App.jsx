238e9bfe769b98b3166e5f1bb12407d1c1c6d681
oldroge <rogeriomrjr@gmail.com>
2022-08-09 21:26:47 -0300

    feat: add a Route for AboutMe component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import AboutMePage from './pages/about-me/AboutMePage';
 
 function App() {
   return (
     <>
-      <AboutMePage />
-      <h1>Portfólio</h1>
+      <Route exact path="/about" component={AboutMePage} />
+      {/* <h1>Portfólio</h1> */}
     </>
   );
 }
