15bfab9588e8fa3fae193a9dc78c4e4648fe6963
oldroge <rogeriomrjr@gmail.com>
2022-08-09 22:16:24 -0300

    feat: add a div with a conver image - porfolio image

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 import Header from '../../components/Header/Header';
 
+import PortfolioImage from './images/portfolio-presents.png';
+
+import './Projects.css';
+
 export default function Projects() {
   return (
     <>
       <Header />
-      <div>Projects</div>
+      <section id="project-page">
+        <div className="container-project">
+          <img
+            src={PortfolioImage}
+            alt="Imagem escrito Portfolio com fundo preto e detalhes de roxo"
+            className="portfolio-image"
+          />
+        </div>
+      </section>
     </>
   );
 }
