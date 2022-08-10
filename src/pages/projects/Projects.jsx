2f9be51a88829ba512db3ef4371518a3eba138c6
oldroge <rogeriomrjr@gmail.com>
2022-08-10 15:38:30 -0300

    feat: create a div image with github icon linked

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 
 import PortfolioImage from './images/portfolio-presents.png';
+import GitHubIcon from './images/icon-github.png';
 
 import './Projects.css';
 
@@ -17,6 +18,19 @@ export default function Projects() {
             alt="Imagem escrito Portfolio com fundo preto e detalhes de roxo"
             className="portfolio-image"
           />
+          <div className="icon-github">
+            <a
+              href="https://github.com/Oldroge/my-personal-portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={GitHubIcon}
+                alt=""
+                className="github-icon"
+              />
+            </a>
+          </div>
         </div>
       </section>
     </>
