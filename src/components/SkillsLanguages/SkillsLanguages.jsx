78fa6508aa7abe2ebdf0b0ce2654221f221120fe
oldroge <rogeriomrjr@gmail.com>
2022-08-13 20:48:14 -0300

    feat: create component for set skill languages

---
+++ src/components/SkillsLanguages/SkillsLanguages.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './SkillsLanguages.css';
+
+export default function SkillsLanguages() {
+  return (
+    <div className="container-skills-languages">
+      <div className="div-language">
+        <div className="languages">a</div>
+        <div className="languages">b</div>
+      </div>
+      <div className="div-language">
+        <div className="languages">c</div>
+        <div className="languages">d</div>
+      </div>
+      <div className="div-language">
+        <div className="languages">e</div>
+        <div className="languages">f</div>
+      </div>
+      <div className="div-language">
+        <div className="languages">g</div>
+        <div className="languages">h</div>
+      </div>
+      <div className="div-language">
+        <div className="languages">i</div>
+        <div className="languages">j</div>
+      </div>
+      <div className="div-language">
+        <div className="languages">k</div>
+        <div className="languages">l</div>
+
+      </div>
+    </div>
+  );
+}
--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Header from '../../components/Header/Header';
+import SkillsLanguages from '../../components/SkillsLanguages/SkillsLanguages';
 
 export default function Skills() {
   return (
     <section>
       <Header />
-      <h1>Skills</h1>
-
+      <SkillsLanguages />
     </section>
   );
 }
