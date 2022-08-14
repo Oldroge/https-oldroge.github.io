d120211fa0ea3139765c8f0b4334764216343eb1
oldroge <rogeriomrjr@gmail.com>
2022-08-13 21:45:27 -0300

    feat: add icons to skills languages

--- src/components/SkillsLanguages/SkillsLanguages.jsx
+++ src/components/SkillsLanguages/SkillsLanguages.jsx
@@ -2,33 +2,99 @@ import React from 'react';
 
 import './SkillsLanguages.css';
 
+import JavaScriptIcon from './images/js.png';
+import HtmlIcon from './images/html-5.png';
+import CssIcon from './images/css.png';
+import ReduxIcon from './images/redux.png';
+import JestIcon from './images/jest.png';
+import AngularIcon from './images/angular.png';
+import ReactIcon from './images/react.png';
+import MySqlIcon from './images/mysql.png';
+import MongoIcon from './images/mongo.png';
+import NodeIcon from './images/nodejs.png';
+
 export default function SkillsLanguages() {
   return (
     <div className="container-skills-languages">
       <div className="div-language">
-        <div className="languages">a</div>
-        <div className="languages">b</div>
+        <div className="languages">
+          <img
+            src={HtmlIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
+        <div className="languages">
+          <img
+            src={CssIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
       </div>
       <div className="div-language">
-        <div className="languages">c</div>
-        <div className="languages">d</div>
+        <div className="languages">
+          <img
+            src={JavaScriptIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
+        <div className="languages">
+          <img
+            src={ReactIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
       </div>
       <div className="div-language">
-        <div className="languages">e</div>
-        <div className="languages">f</div>
+        <div className="languages">
+          <img
+            src={ReduxIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
+        <div className="languages">
+          <img
+            src={JestIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
       </div>
       <div className="div-language">
-        <div className="languages">g</div>
-        <div className="languages">h</div>
+        <div className="languages">
+          <img
+            src={NodeIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
+        <div className="languages">
+          <img
+            src={MySqlIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
       </div>
       <div className="div-language">
-        <div className="languages">i</div>
-        <div className="languages">j</div>
-      </div>
-      <div className="div-language">
-        <div className="languages">k</div>
-        <div className="languages">l</div>
-
+        <div className="languages">
+          <img
+            src={MongoIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
+        <div className="languages">
+          <img
+            src={AngularIcon}
+            alt=""
+            className="icon"
+          />
+        </div>
       </div>
     </div>
   );
