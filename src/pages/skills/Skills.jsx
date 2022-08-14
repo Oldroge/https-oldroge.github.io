e08d533a8c17bebe4efe36d7c7913472e7600ac5
oldroge <rogeriomrjr@gmail.com>
2022-08-13 23:29:18 -0300

    feat: set a div where will be store the text about the language

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -3,11 +3,43 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import SkillsLanguages from '../../components/SkillsLanguages/SkillsLanguages';
 
+import './Skills.css';
+
 export default function Skills() {
   return (
     <section>
       <Header />
-      <SkillsLanguages />
+      <div className="container-skills">
+        <SkillsLanguages />
+        <div className="text-skills">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Excepturi minima animi minus dolorem, rerum deleniti omnis mollitia,
+            eveniet officiis in commodi iure sunt nisi est deserunt aut tenetur obcaecati? Culpa!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Excepturi minima animi minus dolorem, rerum deleniti omnis mollitia,
+            eveniet officiis in commodi iure sunt nisi est deserunt aut tenetur obcaecati? Culpa!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Excepturi minima animi minus dolorem, rerum deleniti omnis mollitia,
+            eveniet officiis in commodi iure sunt nisi est deserunt aut tenetur obcaecati? Culpa!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Excepturi minima animi minus dolorem, rerum deleniti omnis mollitia,
+            eveniet officiis in commodi iure sunt nisi est deserunt aut tenetur obcaecati? Culpa!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Excepturi minima animi minus dolorem, rerum deleniti omnis mollitia,
+            eveniet officiis in commodi iure sunt nisi est deserunt aut tenetur obcaecati? Culpa!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Excepturi minima animi minus dolorem, rerum deleniti omnis mollitia,
+            eveniet officiis in commodi iure sunt nisi est deserunt aut tenetur obcaecati? Culpa!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Excepturi minima animi minus dolorem, rerum deleniti omnis mollitia,
+            eveniet officiis in commodi iure sunt nisi est deserunt aut tenetur obcaecati? Culpa!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Excepturi minima animi minus dolorem, rerum deleniti omnis mollitia,
+            eveniet officiis in commodi iure sunt nisi est deserunt aut tenetur obcaecati? Culpa!
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
