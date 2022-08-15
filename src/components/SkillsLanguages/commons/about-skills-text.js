b2df49c4df901bc4cf370e51846027ed0634550c
oldroge <rogeriomrjr@gmail.com>
2022-08-15 18:37:43 -0300

    feat: add object with texts about the technologies

---
+++ src/components/SkillsLanguages/commons/about-skills-text.js
@@ -0,0 +1,7 @@
+const skills = {
+  html5: 'HTML5 é a quinta e última versão do HTML, sigla de Hypertext Markup Language, uma das primeiras e principais linguagens de marcação da web. O HTML serve, basicamente, para o desenvolvimento de páginas que contenham conteúdos simples, como textos, imagens, áudio, vídeos e etc.',
+  css3: 'CSS3 é a terceira mais nova versão das famosas Cascading Style Sheets, pela qual se define estilos para um projeto web. Com efeitos de transição, imagem, imagem de fundo/background e outros, pode-se criar estilos únicos para seus projetos web, alterando diversos aspectos de design no layout da página.',
+  javascript: 'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.',
+};
+
+export default skills;
