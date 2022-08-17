e3b64b8b75c4edc8f3e3799753aa93bc392bd6b1
oldroge <rogeriomrjr@gmail.com>
2022-08-17 14:30:03 -0300

    feat: add Header component to Contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Header from '../../components/Header/Header';
+
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <>
+      <Header />
+      <h3>Contact</h3>
+    </>
   );
 }
