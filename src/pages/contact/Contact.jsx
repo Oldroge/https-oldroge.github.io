5e57dc6826c41480ede518cdc00e3199004a7065
oldroge <rogeriomrjr@gmail.com>
2022-08-17 14:37:02 -0300

    feat: create a section container and import css file

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import Header from '../../components/Header/Header';
 
+import './Contact.css';
+
 export default function Contact() {
   return (
     <>
       <Header />
-      <h3>Contact</h3>
+      <section className="contact-container">
+        <h3>Para saber mais sobre mim:</h3>
+      </section>
     </>
   );
 }
