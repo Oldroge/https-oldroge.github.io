b9a4d93101487ed20e01bfe2652867ae951a4a97
oldroge <rogeriomrjr@gmail.com>
2022-08-17 14:57:29 -0300

    feat: add a section to set social media images

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -4,13 +4,28 @@ import Header from '../../components/Header/Header';
 
 import './Contact.css';
 
+import GitHubIcon from './images/github.png';
+import LinkedinIcon from './images/linkedin.png';
+import TelegramIcon from './images/telegram.png';
+
 export default function Contact() {
   return (
     <>
       <Header />
-      <section className="contact-container">
+      <article className="contact-container">
         <h3>Para saber mais sobre mim:</h3>
-      </section>
+        <section className="social-medias-container">
+          <div>
+            <img src={LinkedinIcon} alt="" />
+          </div>
+          <div>
+            <img src={GitHubIcon} alt="" />
+          </div>
+          <div>
+            <img src={TelegramIcon} alt="" />
+          </div>
+        </section>
+      </article>
     </>
   );
 }
