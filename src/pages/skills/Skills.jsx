c9c64afa3b53e0087e4454fb3496da0280abbde8
oldroge <rogeriomrjr@gmail.com>
2022-08-20 18:05:17 -0300

    fix: make it text skills page more intuitive

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -4,10 +4,10 @@ import Header from '../../components/Header/Header';
 import SkillsLanguages from '../../components/SkillsLanguages/SkillsLanguages';
 
 import './Skills.css';
-import languageText from '../../components/SkillsLanguages/commons/about-skills-text';
+// import languageText from '../../components/SkillsLanguages/commons/about-skills-text';
 
 export default function Skills() {
-  const [languageName, setLanguageName] = useState(languageText.html5);
+  const [languageName, setLanguageName] = useState('Cliquei em nas linguagens para saber mais');
   // Para conseguir passar informações usando props do componente filho para o pai, usei
   // esta referência:
   // https://www.freecodecamp.org/portuguese/news/como-passar-dados-e-eventos-entre-componentes-em-react/
